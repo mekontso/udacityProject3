cd1be4fa928177c1efa2944725e6778f9a54f198
Mekontso Anderson Sa'a <mekontsoanderson@gmail.com>
2021-10-27 10:31:41 +0100

    Initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udacityproject3
+export POSTGRES_PASSWORD=udacityproject3
+export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=udacityproject3
+export AWS_BUCKET=arn:aws:s3:::udacity-project3-790740712732
+export AWS_REGION=eu-central-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
